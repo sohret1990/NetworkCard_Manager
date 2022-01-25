ee5d2bb841f3dacb510d5bcbac27237bed802b08
Shohrat Suleymanov <sohret.suleymanov@gmail.com>
2022-01-25 16:04:58 +0400

    Scc

--- NetworkCard_Manager/Form1.Designer.cs
+++ NetworkCard_Manager/Form1.Designer.cs
@@ -36,14 +36,14 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(690, 536);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(702, 533);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 536);
+            this.ClientSize = new System.Drawing.Size(702, 533);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "frmMain";
             this.Text = "Ethernet card manager";
